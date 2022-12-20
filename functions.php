71c5be579163d1ec17d9a389301f4a66afe106e2
tedkouuu <99691613+tedkouuu@users.noreply.github.com>
2022-12-20 14:44:33 +0200

    Basic Interior Page

--- functions.php
+++ functions.php
@@ -6,6 +6,13 @@ function university_files(){
     wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
     wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+
 }
 
 add_action("wp_enqueue_scripts", "university_files");
+
+function university_features(){
+    add_theme_support('title-tag');
+}
+
+add_action("after_setup_theme", 'university_features');
