a31825c1dac862a74efe3b3d9cf5bc335a6bc5bb
tedkouuu <99691613+tedkouuu@users.noreply.github.com>
2022-12-20 12:08:37 +0200

    Initial commit

---
+++ header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <?php wp_head(); ?>
+    <title>Fictional University</title>
+</head>
+<body>
+    <h1>Fictional University</h1>
