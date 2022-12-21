186706148944f225f0bc5aba77d30e50f88b3c57
tedkouuu <99691613+tedkouuu@users.noreply.github.com>
2022-12-21 13:19:12 +0200

    Some Improvements

--- single.php
+++ single.php
@@ -4,9 +4,9 @@ get_header();
 
  while (have_posts()) {
     the_post(); ?>
-    <h2><?php the_title();?></h2>
-    <?php the_content(); ?>
- <?php }
+<h2><?php the_title();?></h2>
+<?php the_content(); ?>
+<?php }
 
 get_footer();
 
