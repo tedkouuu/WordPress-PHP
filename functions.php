1ba3cb313cfe3a0039405abe52e545b955dc2476
tedkouuu <99691613+tedkouuu@users.noreply.github.com>
2022-12-22 14:56:47 +0200

    Added Custom Post Type-Events

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@ function university_files(){
     wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
     wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
-
+    
 }
 
 add_action("wp_enqueue_scripts", "university_files");
@@ -16,3 +16,4 @@ function university_features(){
 }
 
 add_action("after_setup_theme", 'university_features');
+
