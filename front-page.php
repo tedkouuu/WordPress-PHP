c875835d6b84172bc3f55ee5ad72c95801d6d4ba
tedkouuu <99691613+tedkouuu@users.noreply.github.com>
2022-12-24 14:36:48 +0200

    Added Event custom fields

--- front-page.php
+++ front-page.php
@@ -29,8 +29,12 @@
                 $homePageEvents->the_post(); ?>
             <div class="event-summary">
                 <a class="event-summary__date t-center" href="#">
-                    <span class="event-summary__month">Apr</span>
-                    <span class="event-summary__day">02</span>
+                    <!-- I installed a plugin to help me with the creation of custom fields. This plugin gives me a new function -->
+                    <span class="event-summary__month"><?php 
+                    $eventDate = new DateTime(get_field('event_date'));
+                    echo $eventDate->format('M')
+                    ?></span>
+                    <span class="event-summary__day"><?php echo $eventDate->format('d')?></span>
                 </a>
                 <div class="event-summary__content">
                     <h5 class="event-summary__title headline headline--tiny"><a
