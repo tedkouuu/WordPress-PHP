437caa38a6d8fc3355fdb1376178172ecc5c3f4a
tedkouuu <99691613+tedkouuu@users.noreply.github.com>
2022-12-26 14:03:51 +0200

    Displaying Relationships (Front-End)

--- single-event.php
+++ single-event.php
@@ -18,10 +18,10 @@ get_header();
 
 <div class="container container--narrow page-section">
 
-<div class="metabox metabox--position-up metabox--with-home-link">
+    <div class="metabox metabox--position-up metabox--with-home-link">
         <p>
-            <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('event'); ?>"><i class="fa fa-home"
-                    aria-hidden="true"></i>Events Home</a> <span
+            <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('event'); ?>"><i
+                    class="fa fa-home" aria-hidden="true"></i>Events Home</a> <span
                 class="metabox__main"><?php the_title();?></span>
         </p>
     </div>
@@ -33,13 +33,21 @@ get_header();
     
     $relatedPrograms = get_field('related_programs');
 
-    foreach($relatedPrograms as $program) {
-        echo get_the_title($program);
+    if ($relatedPrograms){
+        echo '<hr class="section-break">';
+        echo '<h2 class="headline headline--medium">Related Program(s)</h2>';
+        echo '<ul class="link-list min-list">';
+        foreach($relatedPrograms as $program) { ?>
+
+    <li><a href="<?php echo get_the_permalink($program); ?>"><?php echo get_the_title($program); ?></a></li>
+
+    <?php }
+        echo '</ul>';
     }
-    
+
     ?>
 
- </div>
+</div>
 
 
 <?php }
